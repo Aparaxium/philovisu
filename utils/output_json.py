fd7b3ec844e02f5e8290baf07e05f0a5817ad70f
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2021-11-06 00:50:48 -0700

    Added interface to export database data to json for graphing

--- utils/output_json.py
+++ utils/output_json.py
@@ -1,9 +1,31 @@
-import database_sqlite as database
+import utils.database_sqlite as database_sqlite
 import json
+import sys
+
+
+def main(*args):
+    if len(args) != 2:
+        print("Usage: output_json.py <database_file> <output_file>")
+        sys.exit(1)
+
+    db_file = args[0]
+    output_file = args[1]
+
+    db = database_sqlite.database(db_file)
+
+    nodes = db.get_nodes()
+    edges = db.get_edges()
+
+    nodes_f = list(map(lambda x: {"id": x[0], "val": x[1]}, nodes))
+    edges_f = list(map(lambda x: {"source": x[0], "target": x[1]}, edges))
+
+    data = {"nodes": nodes_f, "edges": edges_f}
+
+    # Open the output file
+    with open(output_file, "w") as f:
+        # Convert the data to JSON and write it to the output file
+        json.dump(data, f, indent=4)
 
 
 if __name__ == "__main__":
-    data = {"nodes": database.get_nodes_json(), "edges": database.get_edges_json()}
-    print(data)
-    with open("./data/data.json", "w") as f:
-        json.dump(data, f, indent=4)
+    main(sys.argv[1:])

d56aa3286cc3c745d4057d2cf362ddd9c036ce15
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2021-11-06 00:49:37 -0700

    Added new sqlite interface

--- utils/database_sqlite.py
+++ utils/database_sqlite.py
@@ -1,118 +1,144 @@
 import sqlite3
 import os
 from contextlib import contextmanager
-import logging
+import utils.database
 
 
-@contextmanager
-def create_connection(db_file="./data/database.db"):
-    conn = sqlite3.connect(db_file)
-    try:
-        yield conn
-    except Exception as e:
-        logging.exception(e)
-        if conn is not None:
-            conn.rollback()
-    finally:
-        if conn is not None:
-            conn.close()
+class database(utils.database.database):
+    def __init__(self, db_file="../data/database.db"):
+        self.db_file = db_file
 
+    @contextmanager
+    def __create_connection(self):
+        conn = sqlite3.connect(self.db_file)
+        try:
+            yield conn
+        except sqlite3.Error as e:
+            sqlite3.Error(e)
+            if conn is not None:
+                conn.rollback()
+        finally:
+            if conn is not None:
+                conn.close()
 
-def __execute_write(query, *args):
-    with create_connection() as conn:
-        cur = conn.cursor()
-        cur.execute(query, args)
-        conn.commit()
-        cur.close()
+    def __execute_write(self, query, *args):
+        with self.__create_connection() as conn:
+            try:
+                cur = conn.cursor()
+                cur.execute(query, args)
+                conn.commit()
+            except sqlite3.Error as e:
+                raise sqlite3.Error(e)
+            finally:
+                cur.close()
 
+    def __execute_fetchone(self, query, *args):
+        res = None
+        with self.__create_connection() as conn:
+            try:
+                cur = conn.cursor()
+                cur.execute(query, args)
+                res = cur.fetchone()
+            except sqlite3.Error as e:
+                raise sqlite3.Error(e)
+            finally:
+                cur.close()
+        return res
 
-def __execute_fetchone(query, *args):
-    with create_connection() as conn:
-        cur = conn.cursor()
-        cur.execute(query, args)
-        res = cur.fetchone()
-        cur.close()
-    return res
+    def __execute_fetchall(self, query, *args):
+        with self.__create_connection() as conn:
+            try:
+                cur = conn.cursor()
+                cur.execute(query, args)
+                res = cur.fetchall()
+            except sqlite3.Error as e:
+                raise sqlite3.Error(e)
+            finally:
+                cur.close()
+        return res
 
-def __execute_fetchall(query, *args):
-    with create_connection() as conn:
-        cur = conn.cursor()
-        cur.execute(query, args)
-        res = cur.fetchall()
-        cur.close()
-    return res
+    def __create_table(self, create_table_sql):
+        with self.__create_connection() as conn:
+            try:
+                cur = conn.cursor()
+                cur.execute(create_table_sql)
+            except sqlite3.Error as e:
+                raise sqlite3.Error(e)
+            finally:
+                cur.close()
 
+    def create_database(self):
+        sql_create_page_table = """CREATE TABLE Page (
+            id text PRIMARY KEY
+            );"""
 
-def __create_table(create_table_sql):
-    with create_connection() as conn:
-        cur = conn.cursor()
-        cur.execute(create_table_sql)
-        cur.close()
+        sql_create_influenced_table = """CREATE TABLE "Influence" (
+            "source"	text,
+            "target"	text,
+            FOREIGN KEY("target") REFERENCES "Page"("id"),
+            FOREIGN KEY("source") REFERENCES "Page"("id"),
+            CONSTRAINT "PK_Influenced" PRIMARY KEY("source","target")
+            );"""
 
+        if os.path.exists(self.db_file):
+            os.remove(self.db_file)
 
-def init(db_file="./database.db"):
-    sql_create_page_table = """CREATE TABLE Page (
-        name text PRIMARY KEY
-        );"""
+        try:
+            self.__create_table(sql_create_page_table)
+            self.__create_table(sql_create_influenced_table)
 
-    sql_create_influenced_table = """CREATE TABLE "Influence" (
-        "source"	text,
-        "target"	text,
-        FOREIGN KEY("target") REFERENCES "Page"("name"),
-        FOREIGN KEY("source") REFERENCES "Page"("name"),
-        CONSTRAINT "PK_Influenced" PRIMARY KEY("source","target")
-        );"""
+            # Enable WAL
+            with self.__create_connection() as conn:
+                conn.execute("pragma journal_mode=wal")
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
 
-    if os.path.exists(db_file):
-        os.remove(db_file)
+    def insert_page(self, page_name):
+        """insert a new page into the page table"""
+        sql = """INSERT INTO Page(id)
+                VALUES(?);"""
+        try:
+            self.__execute_write(sql, page_name)
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
 
-    __create_table(sql_create_page_table)
-    __create_table(sql_create_influenced_table)
+    def insert_influenced(self, page_name, influence):
+        """insert a new page into the page table"""
+        sql = """INSERT INTO Influence(source, target)
+                VALUES(?, ?);"""
+        try:
+            self.__execute_write(sql, page_name, influence)
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
 
+    def page_exists(self, page_name):
+        """query data from the a table"""
+        sql = "SELECT EXISTS (SELECT id FROM Page WHERE id=(?))"
+        try:
+            return self.__execute_fetchone(sql, page_name)[0]
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
 
-def insert_page(page_name):
-    """insert a new page into the page table"""
-    sql = """INSERT INTO Page(name)
-             VALUES(?);"""
-    __execute_write(sql, page_name)
+    def influenced_exist(self, page_name, influence):
+        """query data from the a table"""
+        sql = "SELECT EXISTS (SELECT source FROM Influence WHERE source=(?) and target=(?))"
+        try:
+            return self.__execute_fetchone(sql, page_name, influence)[0]
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
 
+    def get_nodes(self):
+        """query data from the a table"""
+        sql = "select page.id, count(source) as val from page left join influence on page.id = influence.source group by page.id order by val desc"
+        try:
+            return self.__execute_fetchall(sql)
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
 
-def insert_influenced(page_name, influence):
-    """insert a new page into the page table"""
-    sql = """INSERT INTO Influence(source, target)
-             VALUES(?, ?);"""
-    __execute_write(sql, page_name, influence)
-
-
-def page_exists(page_name):
-    """query data from the a table"""
-    sql = "SELECT EXISTS (SELECT name FROM Page WHERE name=(?))"
-    return __execute_fetchone(sql, page_name)[0]
-
-
-def influenced_exist(page_name, influence):
-    """query data from the a table"""
-    sql = (
-        "SELECT EXISTS (SELECT name FROM Influence WHERE source=(?) and target=(?))"
-    )
-    return __execute_fetchone(sql, page_name, influence)[0]
-
-
-def get_nodes_json():
-    """query data from the a table"""
-    sql = "select page.name as id, count(source) as val from page left join influence on page.name = influence.source group by page.name order by val desc"
-    with create_connection() as conn:
-        cur = conn.cursor()
-        cur.execute(sql)
-        r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
-    return r
-
-
-def get_edges_json():
-    """query data from the a table"""
-    sql = "SELECT * from influence"
-    with create_connection() as conn:
-        cur = conn.cursor()
-        cur.execute(sql)
-        r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
-    return r
+    def get_edges(self):
+        """query data from the a table"""
+        sql = "SELECT * from influence"
+        try:
+            return self.__execute_fetchall(sql)
+        except sqlite3.Error as e:
+            raise sqlite3.Error(e)
